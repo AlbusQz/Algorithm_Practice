ee0adc65d2e5ef34f28b36c86e28c3838b8740de
AlbusQz <albusqz@163.com>
2024-05-18 21:38:29 +0800

    solve lc_9 in python using stirng method

--- getting_started/9.回文数.py
+++ getting_started/9.回文数.py
@@ -14,12 +14,27 @@ class Solution:
     def isPalindrome(self, x: int) -> bool:
         if x < 0:
             return False
+        
         # using int only
+        '''
         y = 0
         _ = x
         while x > 0:
             y = 10*y + x%10
             x = x//10
         return y==_
+        '''
+        #using string
+        x = str(x)
+        n = len(x)
+        i = 0
+        j = n-1
+        while i <= j:
+            if x[i]!=x[j]:
+                return False
+            else:
+                i += 1
+                j -= 1
+        return True
 # @lc code=end
 
