da159b1749363c2f21098ee6d00ae78c7d26d264
AlbusQz <albusqz@163.com>
2024-05-18 21:34:56 +0800

    solve lc_9 in python without tranform x to string

---
+++ getting_started/9.回文数.py
@@ -0,0 +1,25 @@
+# @before-stub-for-debug-begin
+from python3problem9 import *
+from typing import *
+# @before-stub-for-debug-end
+
+#
+# @lc app=leetcode.cn id=9 lang=python3
+#
+# [9] 回文数
+#
+
+# @lc code=start
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+        # using int only
+        y = 0
+        _ = x
+        while x > 0:
+            y = 10*y + x%10
+            x = x//10
+        return y==_
+# @lc code=end
+
