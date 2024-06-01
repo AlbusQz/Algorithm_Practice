bb4a1c5652592c8aa79a44c189f2c795e0607b97
AlbusQz <albusqz@163.com>
2024-06-02 01:56:06 +0800

    finish lc_15 with leetcode's help(double pointer)

--- getting_started/15.三数之和.py
+++ getting_started/15.三数之和.py
@@ -12,7 +12,42 @@ from typing import *
 # @lc code=start
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        ##
+        ##with leetcode's help(double pointer)
+        result = []
+        nums.sort()
+        n = len(nums)
+        for i in range(n-1):
+            if i>0 and nums[i] == nums[i-1]:
+                continue
+            else:
+                k = n - 1
+                for j in range(i+1,n-1):
+                    if (j!= i+1 and nums[j] == nums[j-1]) or nums[i] + nums[j] + nums[k] < 0:
+                        # j += 1
+                        continue
+                    while j<k and nums[i] + nums[j] + nums[k] >0 :
+                        k -= 1
+                    if not(j<k):
+                        break
+                    elif nums[i] + nums[j] + nums[k] == 0 :
+                        result.append([nums[i],nums[j],nums[k]])
+                        continue
+                '''
+                j = i+1
+                k = n-1
+                while j<k:
+                    if (j!= i+1 and nums[j] == nums[j-1]) or nums[i] + nums[j] + nums[k] < 0:
+                        j += 1
+                        continue
+                    elif (k!= n-1 and nums[k] == nums[k+1]) or nums[i] + nums[j] + nums[k] > 0:
+                        k -= 1
+                        continue
+                    else:
+                        result.append([nums[i],nums[j],nums[k]])
+                        j += 1
+                        k -= 1  '''
+        return result
+                
         
         ## brute force
         '''
