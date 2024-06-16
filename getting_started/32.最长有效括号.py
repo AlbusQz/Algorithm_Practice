b44bf32b3225c89840b941665ebd6f5f84c78ed4
AlbusQz <albusqz@163.com>
2024-06-16 15:37:20 +0800

    adding dp_method from lc_solution

--- getting_started/32.最长有效括号.py
+++ getting_started/32.最长有效括号.py
@@ -12,36 +12,63 @@ from typing import *
 # @lc code=start
 class Solution:
     def longestValidParentheses(self, s: str) -> int:
-        n = len(s)
-        flags = [False for _ in range(n)]
         
-        stack = []
-        index = []
-        
-        result = 0
-        top = -1
-        for i in range(n):
-            if s[i] == '(':
-                stack.append('(')
-                index.append(i)
-                top += 1
-                # index += 1
-            elif s[i] == ')':
-                if top>=0 and stack[top] == '(':
-                    stack.pop()
-                    temp_index = index.pop()
-                    flags[i] = True
-                    flags[temp_index] = True
+        def my_method(s):
+            n = len(s)
+            flags = [False for _ in range(n)]
+            
+            stack = []
+            index = []
+            
+            result = 0
+            top = -1
+            for i in range(n):
+                if s[i] == '(':
+                    stack.append('(')
+                    index.append(i)
+                    top += 1
+                    # index += 1
+                elif s[i] == ')':
+                    if top>=0 and stack[top] == '(':
+                        stack.pop()
+                        temp_index = index.pop()
+                        flags[i] = True
+                        flags[temp_index] = True
 
-                    top -= 1
+                        top -= 1
+            
+            temp_result = 0
+            for i in range(n):
+                if flags[i] == True:
+                    temp_result += 1
+                    result = max(result,temp_result)
+                else:
+                    temp_result = 0
+            return result
         
-        temp_result = 0
-        for i in range(n):
-            if flags[i] == True:
-                temp_result += 1
-                result = max(result,temp_result)
-            else:
-                temp_result = 0
-        return result
+        #adding dp method
+        def dp_method(s):
+            
+            n = len(s)
+            if n == 0:
+                return 0
+            dp = [0 for _ in range(n)]
+            for i in range(1,n):
+                if s[i] == ')':
+                    if s[i-1]== '(':
+                        if i-2 >=0:
+                            dp[i] = dp[i-2] + 2
+                        else:
+                            dp[i] = 2
+                    else:
+                        if i-dp[i-1]-1 >=0 and s[i-dp[i-1]-1] == '(':
+                            if i-dp[i-1]-2 >= 0:
+                                dp[i] = dp[i-1]+dp[i-dp[i-1]-2]+2
+                            else:
+                                dp[i] = dp[i-1]+2
+            # print(dp)
+            return max(dp)
+            
+        return dp_method(s)
 # @lc code=end
 
