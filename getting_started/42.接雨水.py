dc836b1b7f693a94ea53250148fb9f7912ef7772
AlbusQz <albusqz@163.com>
2024-10-03 19:31:41 +0800

    添加了42题的其他两种解法

---
+++ getting_started/42.接雨水.py
@@ -0,0 +1,110 @@
+#
+# @lc app=leetcode.cn id=42 lang=python3
+#
+# [42] 接雨水
+#
+
+# @lc code=start
+class Solution:
+    def trap(self, height: List[int]) -> int: 
+        
+        ## 双指针法,优化掉了原来需要的存储空间
+        result = 0
+        n = len(height)
+        left = 0
+        right = n-1
+        leftmax = height[0]
+        rightmax = height[-1]
+        
+        while left < right:
+            leftmax = max(leftmax,height[left])
+            rightmax = max(rightmax,height[right])
+            
+            if leftmax < rightmax:
+                
+                result += leftmax - height[left]
+                left += 1
+                
+            else:
+                
+                result += rightmax - height[right]
+                right -= 1
+        return result
+        
+        
+        
+        
+        ## DP法
+        '''
+        n = len(height)
+        left = []
+        right = []
+        # left.append(height[0])
+
+        before = 0
+        for i in range(n):
+            if height[i] > before:
+                before = height[i]
+            left.append(before)
+        
+        before = 0
+        for i in range(n-1,-1,-1):
+            if height[i]>before:
+                before = height[i]
+            right.insert(0,before)
+        
+        result = []
+        for i in range(0,n):
+            result.append(min(left[i],right[i])-height[i])
+        result = sum(result)
+        return result
+        '''
+        
+
+        
+# @lc code=end
+
+        ## 暴力法，会超时
+        '''
+        n = len(height)
+        
+        def count_v(h):
+            result = 0
+            flags = []
+            begin = 0
+            b_flag = False
+            for i in range(n):
+                if height[i] < h :
+                    flags.append(True)
+                    if b_flag == False:
+                        begin = i+1
+                else:
+                    flags.append(False)
+                    b_flag = True
+                    end = i+1
+            # begin = 0
+            # end = n
+            # print(begin)
+            # while flags[begin] == True and begin < end:
+            #     begin += 1
+            # while flags[end-1] == True and end > begin:
+            #     end -= 1
+
+            for i in range(begin,end):
+                if flags[i] == True:
+                    result += 1
+            return result
+                    
+            
+        
+        result = 0
+        
+        
+        height_max = max(height)
+        
+        for i in range(height_max):
+            result += count_v(i+1)
+            # print(count_v(i+1))
+        
+        return result
+        '''
