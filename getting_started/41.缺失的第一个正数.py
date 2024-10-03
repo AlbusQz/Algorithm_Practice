af24c4a28aa6eadb48538bc5d0ee7afb13f0e6f9
AlbusQz <albusqz@163.com>
2024-10-03 14:46:49 +0800

    明白了，题的要求看错了，修改了代码

--- getting_started/41.缺失的第一个正数.py
+++ getting_started/41.缺失的第一个正数.py
@@ -7,7 +7,28 @@
 # @lc code=start
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
+        ##用来负数的方式做标记，很巧妙，就可以避免标记带来的信息丢失
         
+        n = len(nums)
+        
+        for i in range(n):
+                if nums[i]<=0 :
+                    nums[i] = n+1 
+        
+        for i in range(n):
+            temp = abs(nums[i])
+            if temp <= n:
+                nums[temp-1] = - abs(nums[temp-1])
+        
+        for i in range(n):
+            if nums[i]>0:
+                return i+1
+            
+        return n+1
+                
+        
+        ## 看错要求了，要求o(1)的空间复杂度，我用了o(n)的
+        '''
         n = len(nums)
         prepare = [False for _ in range(n)]
         
@@ -21,6 +42,7 @@ class Solution:
                 return i+1
             
         return n+1
+        '''
             
         
 # @lc code=end
