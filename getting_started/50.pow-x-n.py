3dd4646fa4a37a6aad88efbb6455e0007e54c4b0
AlbusQz <albusqz@163.com>
2024-10-08 10:20:22 +0800

    50题的迭代二进制算法

--- getting_started/50.pow-x-n.py
+++ getting_started/50.pow-x-n.py
@@ -8,6 +8,40 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
         
+        ## 迭代二进制算法
+        if x == 0 and n > 0:
+            return 0.0
+        if n == 0 and x != 0:
+            return 1.0
+        if x == 1:
+            return 1.0
+        if n == 1:
+            return x
+        if x == -1:
+            if n%2 == 0:
+                return 1
+            else:
+                return -1
+        
+        flag = False
+        if n < 0:
+            flag = True
+            n = -n
+        result = 1
+        while n>0:
+            temp = n%2
+            n = int(n/2)
+            if temp >0:
+                result *= x
+            x *= x
+        if flag:
+            result = 1/result
+        return result
+            
+        
+        
+        ## 暴力法只为解题，其实学不到什么东西
+        '''
         if x == 0 and n > 0:
             return 0.0
         if n == 0 and x != 0:
@@ -34,5 +68,6 @@ class Solution:
                 result = result*x
             result = 1/result
         return result
+        '''
 # @lc code=end
 
