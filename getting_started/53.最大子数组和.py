a3814329306f302161170fb24effaa34c70d9919
AlbusQz <albusqz@163.com>
2024-10-10 08:47:12 +0800

    53,dp

---
+++ getting_started/53.最大子数组和.py
@@ -0,0 +1,21 @@
+#
+# @lc app=leetcode.cn id=53 lang=python3
+#
+# [53] 最大子数组和
+#
+
+# @lc code=start
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        # max = -1*10000
+        maxSum = []
+        n = len(nums)
+        maxSum.append(nums[0])
+        for i in range(n-1):
+            if maxSum[-1]>0:
+                maxSum.append(maxSum[-1] + nums[i+1])
+            else:
+                maxSum.append(nums[i+1])
+        return max(maxSum)
+# @lc code=end
+
