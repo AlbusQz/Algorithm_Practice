3aba6956a86438e84895e3ea911acfbbb9b489ee
AlbusQz <albusqz@163.com>
2024-10-12 09:54:22 +0800

    62题添加了dp法

--- getting_started/62.不同路径.py
+++ getting_started/62.不同路径.py
@@ -6,9 +6,9 @@
 
 # @lc code=start
 class Solution:
-    def uniquePaths(self, m: int, n: int) -> int:
-        
+    def uniquePaths(self, m: int, n: int) -> int:  
         ## 排列组合法
+        '''
         top = m + n - 2
         bottom = 1
         result = 1
@@ -18,5 +18,20 @@ class Solution:
             top -= 1 
             bottom +=1
         return int(result)
+        '''
+        ## 看了题解后知道有的dp法，dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        ## 即到达一个位置的路径数量等于到他上面和左面的位置的路径数量的和
+        dp = [[0 for _ in range(n)]for _ in range(m)]
+        dp[0][0] = 1
+        for i in range(m):
+            for j in range(n):
+                if i >= 1:
+                    dp[i][j] += dp[i-1][j]
+                if j >= 1:
+                    dp[i][j] += dp[i][j-1]
+        # print(dp)
+        return dp[-1][-1]
+        
+    
 # @lc code=end
 
