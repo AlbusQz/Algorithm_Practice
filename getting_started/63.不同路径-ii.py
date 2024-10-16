3c2333998f032bd7787ce005d7d6bf542a24acb5
AlbusQz <albusqz@163.com>
2024-10-16 14:11:07 +0800

    63,dp法

---
+++ getting_started/63.不同路径-ii.py
@@ -0,0 +1,36 @@
+#
+# @lc app=leetcode.cn id=63 lang=python3
+#
+# [63] 不同路径 II
+#
+
+# @lc code=start
+class Solution:
+    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        
+        if obstacleGrid[0][0] == 1:
+            return 0
+        
+        m = len(obstacleGrid)
+        n = len(obstacleGrid[0])
+        
+        if obstacleGrid[m-1][n-1] == 1:
+            return 0
+        
+        dp = [[0 for _ in range(n)] for _ in range(m)]
+        dp[0][0] = 1
+        
+        for i in range(m):
+            for j in range(n):
+                if obstacleGrid[i][j] == 1:
+                    dp[i][j] = 0
+                else:
+                    if i > 0:
+                        dp[i][j] += dp[i-1][j]
+                    if j > 0:
+                        dp[i][j] += dp[i][j-1]
+        
+        return dp[m-1][n-1]
+        
+# @lc code=end
+
