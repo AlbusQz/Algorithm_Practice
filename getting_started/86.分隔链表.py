a802e3064cfc8efd9a681a3c183fad1d0d4cec3f
AlbusQz <albusqz@163.com>
2024-11-03 20:14:35 +0800

    86题秒了

--- getting_started/86.分隔链表.py
+++ getting_started/86.分隔链表.py
@@ -13,5 +13,28 @@
 class Solution:
     def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
         
+        new_head_prev = ListNode(-1)
+        new_head_prev.next = None
+        new_head = new_head_prev
+        mid_prev = ListNode(-1)
+        mid_prev.next = None
+        mid = mid_prev
+
+        
+        now = head
+        while now is not None:
+            if now.val < x:
+                new_head.next = now
+                new_head = now
+
+            else:
+                mid.next = now
+                mid = now
+            now = now.next
+        
+        new_head.next = mid_prev.next
+        mid.next = None
+        return new_head_prev.next
+            
 # @lc code=end
 
