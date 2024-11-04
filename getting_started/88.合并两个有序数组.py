41c3d0999a9d29c4f924afb09053a7d82b849f1b
AlbusQz <albusqz@163.com>
2024-11-04 15:33:25 +0800

    88题，增加了逆向双指针法，可以省去辅助数组的空间复杂度。“所有玩家都全力向前冲刺, 却不知道向后才是胜利之门。-《头号玩家》”

--- getting_started/88.合并两个有序数组.py
+++ getting_started/88.合并两个有序数组.py
@@ -10,8 +10,31 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
+        ## 逆向双指针，可以省去辅助数组的空间复杂度
+        tail = m + n - 1
+        i = m - 1
+        j = n - 1
+        while i > -1 and j > -1:
+            if nums1[i] > nums2[j]:
+                nums1[tail] = nums1[i]
+                i -= 1
+            else:
+                nums1[tail] = nums2[j]
+                j -= 1
+            tail -= 1
+        while i > -1:
+            nums1[tail] = nums1[i]
+            i -= 1
+            tail -= 1
+        while j > -1:
+            nums1[tail] = nums2[j]
+            j -= 1
+            tail -= 1
+        return     
+        
         
         ## 双指针法
+        '''
         nums3 = []
         i = 0
         j = 0
@@ -32,6 +55,6 @@ class Solution:
                 j += 1
         for i in range(m+n):
             nums1[i] = nums3[i]
-        
+        '''
 # @lc code=end
 
